56615ea02fe4580e8791df02f9749d6f533cb6ca
alona19 <76895924+alona19@users.noreply.github.com>
2024-04-24 22:14:45 +0530

    Update preview.tf

--- preview/preview.tf
+++ preview/preview.tf
@@ -136,6 +136,37 @@ resource "aws_instance" "web004" {
   tags = {
     Name = "Web004"
   }
+  user_data = <<-EOF
+              #!/bin/bash
+              echo 'Adding script.sh...'
+              cat > /tmp/script.sh << 'EOL'
+              #!/bin/bash
+              # Add Docker's official GPG key:
+              sudo apt-get update
+              sudo apt-get install ca-certificates curl
+              sudo install -m 0755 -d /etc/apt/keyrings
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+              sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+              # Add the repository to Apt sources:
+              echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update
+
+              # Install Docker CE
+              sudo apt-get install -y docker-ce
+
+              # Add the ubuntu user to the docker group
+              sudo usermod -aG docker ubuntu
+
+              # Run a Docker container
+              sudo docker run -d --name my-container -p 80:80 nginx
+              EOL
+              chmod +x /tmp/script.sh
+              /tmp/script.sh
+              EOF
 }
 
 
