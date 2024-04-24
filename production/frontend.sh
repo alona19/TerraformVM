0c14e839579c69aec86c653842c137d281391471
alona19 <76895924+alona19@users.noreply.github.com>
2024-04-25 00:32:32 +0530

    Update frontend.sh

--- production/frontend.sh
+++ production/frontend.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Save the script to a file named "frontend.sh"
-cat << 'EOF' > frontend.sh
-#!/bin/bash
-echo 'Adding frontend.sh...'
-
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the Docker repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+# Add the repository to Apt sources:
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
@@ -28,10 +23,3 @@ sudo docker login -u alonageorge25219 -p Jeal@1908
 
 # Run a Docker container
 sudo docker run -d --name my-container -p 80:80 alonageorge25219/frontend:1.0.1
-EOF
-
-# Make the script executable
-chmod +x frontend.sh
-
-# Execute the script
-./frontend.sh
